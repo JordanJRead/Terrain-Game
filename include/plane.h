d51d6cbb7991a333e90697dd9dc80767b561fba1
Jordan Read <jordan.j.read@gmail.com>
2025-08-07 19:25:07 -0400

    Started work on physics planes.

--- include/plane.h
+++ include/plane.h
@@ -4,20 +4,23 @@
 #include "glad/glad.h"
 #include "glm/glm.hpp"
 #include <vector>
+#include <functional>
 
 class Plane {
 public:
 	Plane(int verticesPerEdge);
+	Plane(const Plane& basePlane, const glm::vec3 worldPos, float scale, std::function<float(const glm::vec2&)> heightFunction);
 	int getIndexCount() const { return mIndexCount; }
 	int getVerticesPerEdge() const { return mVerticesPerEdge; }
 	float getStepSize() const { return 1.0 / (mVerticesPerEdge - 1); }
-	const static int mAttribs{ 2 };
-	const std::vector<float>& getVertices() { return mVertices; }
-	const std::vector<unsigned int>& getIndices() { return mIndices; }
+	int getFloatsPerVertex() const { return mFloatsPerVertex; }
+	const std::vector<float>& getVertices() const { return mVertices; }
+	const std::vector<unsigned int>& getIndices() const { return mIndices; }
 
 private:
 	int mVerticesPerEdge;
 	int mIndexCount;
+	int mFloatsPerVertex;
 	std::vector<float> mVertices;
 	std::vector<unsigned int> mIndices;
 };
--- src/plane.cpp
+++ src/plane.cpp
@@ -3,15 +3,15 @@
 #include "plane.h"
 #include "random.h"
 #include <iostream>
+#include <cassert>
 
 Plane::Plane(int verticesPerEdge)
 	: mVerticesPerEdge{ verticesPerEdge }
+	, mFloatsPerVertex{ 2 }
 {
 	int vertexCount{ mVerticesPerEdge * mVerticesPerEdge };
 
-	int floatsPerVertex{ mAttribs };
-
-	mVertices.reserve(vertexCount * floatsPerVertex);
+	mVertices.reserve(vertexCount * mFloatsPerVertex);
 
 	double stepSize{ 1.0 / (mVerticesPerEdge - 1) };
 	double currentXPos{ -0.5 };
@@ -49,5 +49,48 @@ Plane::Plane(int verticesPerEdge)
 		mIndices.push_back(vertexI + 1 + mVerticesPerEdge);
 
 		++quadsFound;
+	}
+}
+
+Plane::Plane(const Plane& basePlane, const glm::vec3 worldPos, float scale, std::function<float(const glm::vec2&)> heightFunction)
+	: mVerticesPerEdge{ basePlane.getVerticesPerEdge() }
+	, mIndexCount{ basePlane.getIndexCount() }
+	, mFloatsPerVertex{ 3 }
+	, mIndices{ basePlane.getIndices() }
+{
+	assert(basePlane.getFloatsPerVertex() == 2);
+
+	int vertexCount{ mVerticesPerEdge * mVerticesPerEdge };
+
+	mVertices.reserve(vertexCount * mFloatsPerVertex);
+
+	const std::vector<float>& baseVertexData{ basePlane.getVertices() };
+	glm::vec2 flatStepWorldPos;
+
+	for (int baseVertexI{ 0 }; baseVertexI < baseVertexData.size() - 1; baseVertexI += 2) {
+		float xValue{ baseVertexData[baseVertexI] };
+		float zValue{ baseVertexData[baseVertexI + 1] };
+		xValue *= scale; // preserve step size????
+		xValue += flatStepWorldPos.x;
+		zValue *= scale;
+		zValue += flatStepWorldPos.y;
+		float yValue = heightFunction({ xValue, zValue });
+	}
+
+	for (int baseVertexI{ 0 }; baseVertexI < baseVertexData.size(); ++baseVertexI) {
+		bool isX{ baseVertexI % 2 == 0 };
+		float value{ baseVertexData[baseVertexI] };
+		value *= scale;
+		value += isX ? flatStepWorldPos.x : flatStepWorldPos.y;
+		if (isX) {
+			float zValue{ baseVertexData[baseVertexI] };
+			value *= scale;
+			value += isX ? flatStepWorldPos.x : flatStepWorldPos.y;
+			mVertices.push_back(value);
+			mVertices.push_back(heightFunction({ baseVertexData[baseVertexI], baseVertexData[baseVertexI + 1] })); // add y
+		}
+		else { // z coord
+			mVertices.push_back(baseVertexData[baseVertexI]);
+		}
 	}
 }
